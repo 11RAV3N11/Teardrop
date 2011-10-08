ed7b0d9975e665d1ab2d7f5f1f86f2cb92ba1693
Andrew Polaskey <apolaskey@gmail.com>
2011-10-08 12:45:49 -0400

    Forgot to add changed files =E:/Git

---
+++ Experia Framework/Sprites/Sprite.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Experia.Framework
+{
+    public class Sprite
+    {
+        public Texture2D Texture;
+        public Vector2 Position, Origin, Scale;
+        public float Rotation, Layer;
+        public BoundingBox BoundingBox;
+        public Color Color;
+        public SpriteEffects SpriteEffects;
+
+        public Sprite()
+        {
+            Scale = Vector2.One;
+            Color = Color.White;
+            SpriteEffects = SpriteEffects.None;
+            BoundingBox = new BoundingBox();
+        }
+    }
+}
