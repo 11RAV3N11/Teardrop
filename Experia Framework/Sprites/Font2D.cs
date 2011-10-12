54d21b70ec4405b79a0cad3e5cb53436135e1164
Andrew Polaskey <apolaskey@gmail.com>
2011-10-12 13:11:14 -0400

    Chiller spritefont test file and the new Font2D wrapper.

---
+++ Experia Framework/Sprites/Font2D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Experia.Framework
+{
+    public class Font2D
+    {
+        SpriteFont m_Font;
+        public Color Color;
+        public float Rotation, Layer;
+        public Vector2 Origin, Scale;
+        public SpriteEffects Effects;
+        public Font2D(string spriteFontLocation)
+        {
+            m_Font = ContentLoader.Instance.Load<SpriteFont>(Container.Persistent, spriteFontLocation);
+            Color = Color.White;
+            Origin = Vector2.Zero;
+            Scale = Vector2.One;
+            Effects = SpriteEffects.None;
+        }
+        public Vector2 MeasureString(object text)
+        {
+            string tempString = text as string;
+            StringBuilder tempStringBuilder = text as StringBuilder;
+
+            if (tempString != null)
+                return m_Font.MeasureString(tempString);
+            else if (tempStringBuilder != null)
+                return m_Font.MeasureString(tempStringBuilder);
+
+            return Vector2.Zero;
+        }
+        public void Draw(SpriteBatch spriteBatch, string text, Vector2 position)
+        {
+            spriteBatch.DrawString(m_Font, text, position, Color, Rotation, Origin, Scale, Effects, Layer);
+        }
+
+    }
+}
