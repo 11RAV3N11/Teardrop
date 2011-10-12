c5c5f40c607746a384df23939f326f09ea517e98
Ricardo Brito <rbrito89@gmail.com>
2011-10-12 15:01:48 -0400

    adding in sound

---
+++ Experia Framework/Sound/AudioEngine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Sound_Engine
+{
+    public class AudioEngine
+    {
+        public static AudioEngine Instance { get { return Experia.Framework.Generics.Singleton<AudioEngine>.Instance; } }
+
+        private float _MasterVolume = 1.0f;
+        public float MasterVolume
+        {
+            get { return _MasterVolume; }
+            set
+            {
+                _MasterVolume = MathHelper.Clamp(value, 0.0f, 1.0f);
+            }
+        }
+    }
+}
