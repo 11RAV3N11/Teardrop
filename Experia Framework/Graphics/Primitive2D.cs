056b9cb3d3c2612de5535a12d9952898cacaeba7
Andrew Polaskey <apolaskey@gmail.com>
2011-10-16 17:59:53 -0400

    Added in Primitive2D class, no logic added yet.

---
+++ Experia Framework/Graphics/Primitive2D.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Experia.Framework.Graphics
+{
+    public class Primitive2D
+    {
+        public static Primitive2D Instance { get { return Experia.Framework.Generics.Singleton<Primitive2D>.Instance; } }
+        protected Primitive2D() { }
+
+    }
+}
