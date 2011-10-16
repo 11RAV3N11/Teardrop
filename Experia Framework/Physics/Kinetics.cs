9379ca11fbc2676593f0008ac144b328ec6188be
Andrew Polaskey <apolaskey@gmail.com>
2011-10-16 18:21:02 -0400

    Merged CRamirez Physics into Origin.

---
+++ Experia Framework/Physics/Kinetics.cs
@@ -0,0 +1,39 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Experia.Framework.Physics;
+
+namespace Experia.Framework.Entities
+{
+    public class Kinetics
+    {
+        protected Vector2 m_PreviousPosition;
+        protected Vector2 m_NewPosition;
+        protected Vector2 m_ValidPosition;
+        protected BaseDrawableGameEntity2D m_CheckObjectEntity;
+        //protected Sprite m_CheckObjectSprite;
+        public static Kinetics Instance
+        {
+            get
+            {
+                return Experia.Framework.Generics.Singleton<Kinetics>.Instance;
+            }
+        }
+        public Vector2 MoveObject(BaseDrawableGameEntity2D entityObject, Vector2 objectPosition, Vector2 futurePosition)
+        {
+            m_CheckObjectEntity = entityObject;
+            //m_CheckObjectSprite = objectSprite;
+            m_PreviousPosition = objectPosition;
+            m_NewPosition = futurePosition;
+            if (m_PreviousPosition == m_NewPosition)
+            {
+                m_ValidPosition = m_NewPosition;
+                return m_ValidPosition;
+            }
+            ObjectCollision.Instance.RunPostitionCollision(m_CheckObjectEntity);
+            return m_ValidPosition;
+        }
+    }
+}
---
+++ Experia Framework/Physics/ObjectCollision.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Experia.Framework.Entities;
+
+namespace Experia.Framework.Physics
+{
+    class ObjectCollision
+    {
+        protected Vector2 m_ValidatedPosition;
+        protected BaseDrawableGameEntity2D m_BaseObjectEntity;
+        protected BaseDrawableGameEntity2D m_TestObjectEntity;
+        protected Rectangle m_BaseObjectRectangle;
+        protected Rectangle m_TestObjectRectangle;
+        protected int m_EntityCountainerCount;
+        public static ObjectCollision Instance
+        {
+            get
+            {
+                return Experia.Framework.Generics.Singleton<ObjectCollision>.Instance;
+            }
+        }
+        protected void UpdateInformation()
+        {
+            m_EntityCountainerCount = EntityManager.Instance.DrawableGameObjects.Count;
+        }
+        public Vector2 RunPostitionCollision(BaseDrawableGameEntity2D objectEntity)
+        {
+            UpdateInformation();
+            return m_ValidatedPosition;
+        }
+    }
+}
